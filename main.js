7c39a4bf00d61ebc08b4500957de3dc6a6a29d40
Ruan Lima <ruanpereiralima@gmail.com>
2021-07-01 23:37:13 -0300

    Update main.js
    show button fix

--- main.js
+++ main.js
@@ -1,55 +1,53 @@
-/* abre fecha o menu quando clicar no icone: hamburguer e x */
-
-const nav = document.querySelector ('#header nav')
-const toggle = document.querySelectorAll ('nav .toggle')
+/*  abre e fecha o menu quando clicar no icone: hamburguer e x */
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
 
 for (const element of toggle) {
-    element.addEventListener ('click', function() {
-        nav.classList.toggle('show')
-    })
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
 }
 
 /* quando clicar em um item do menu, esconder o menu */
-const links = document.querySelectorAll ('nav ul li a')
+const links = document.querySelectorAll('nav ul li a')
 
 for (const link of links) {
-    link.addEventListener('click', function() {
-        nav.classList.remove('show')
-    })
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
 }
 
-/* mudar o header da pagina quando der scroll */
+/* mudar o header da página quando der scroll */
 function changeHeaderWhenScroll() {
-    const header = document.querySelector('#header')
-    const navHeight = header.offsetHeight
-  
-    if (window.scrollY >= navHeight) {
-      // scroll é maior que a altura do header
-      header.classList.add('scroll')
-    } else {
-      // menor que a altura do header
-      header.classList.remove('scroll')
-    }
+  const header = document.querySelector('#header')
+  const navHeight = header.offsetHeight
+
+  if (window.scrollY >= navHeight) {
+    // scroll é maior que a altura do header
+    header.classList.add('scroll')
+  } else {
+    // menor que a altura do header
+    header.classList.remove('scroll')
   }
+}
+
 /* Testimonials carousel slider swiper */
-
 const swiper = new Swiper('.swiper-container', {
-   slidesPerView: 1,
-   pagination: {
-       el: '.swiper-pagination'
-   },
-   mousewheel: true,
-   keyboard: true,
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
+})
 
-  });
-
-/* ScrollReveal: Mostrar elementos quando der scroll na página*/ 
+/* ScrollReveal: Mostrar elementos quando der scroll na página */
 const scrollReveal = ScrollReveal({
-    origin: 'top',
-    distance: '40px',
-    duration: 700,
-    reset: true
-  })
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
 
 scrollReveal.reveal(
   `#home .image, #home .text,
@@ -61,14 +59,20 @@ scrollReveal.reveal(
   `,
   { interval: 100 }
 )
-  
+
 /* Botão voltar para o topo */
 function backToTop() {
-    const backToTopButton = document.querySelector('.back-to-top')
-  
-    if (window.scrollY >= 560) {
-      backToTopButton.classList.add('show')
-    } else {
-      backToTopButton.classList.remove('show')
-    }
+  const backToTopButton = document.querySelector('.back-to-top')
+
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
   }
+}
+
+/* When Scroll */
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backToTop()
+})
