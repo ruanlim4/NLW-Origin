481eeb943d7c367266076962ffdb2f82c3fd04d2
Ruan Lima <ruanpereiralima@gmail.com>
2021-07-01 23:17:16 -0300

    Commit 01/072021
    Footer and Back-To-Top button
    23:15

--- main.js
+++ main.js
@@ -19,20 +19,18 @@ for (const link of links) {
 }
 
 /* mudar o header da pagina quando der scroll */
-
-const header = document.querySelector ('#header')
-const navHeight = header.offsetHeight
-
-window.addEventListener ('scroll', function (){
-    if(this.window.scrollY >= navHeight) {
-        // scroll é maior que a altura do header
-        header.classList.add('scroll')
+function changeHeaderWhenScroll() {
+    const header = document.querySelector('#header')
+    const navHeight = header.offsetHeight
+  
+    if (window.scrollY >= navHeight) {
+      // scroll é maior que a altura do header
+      header.classList.add('scroll')
     } else {
-        // menor que a altura do header
-        header.classList.remove('scroll')
+      // menor que a altura do header
+      header.classList.remove('scroll')
     }
-})
-
+  }
 /* Testimonials carousel slider swiper */
 
 const swiper = new Swiper('.swiper-container', {
@@ -58,7 +56,19 @@ scrollReveal.reveal(
   #about .image, #about .text,
   #services header, #services .card,
   #testimonials header, #testimonials .testimonials
-  #contact .text, #contact .links
+  #contact .text, #contact .links,
+  footer .brand, footer .social
   `,
   { interval: 100 }
 )
+  
+/* Botão voltar para o topo */
+function backToTop() {
+    const backToTopButton = document.querySelector('.back-to-top')
+  
+    if (window.scrollY >= 560) {
+      backToTopButton.classList.add('show')
+    } else {
+      backToTopButton.classList.remove('show')
+    }
+  }
